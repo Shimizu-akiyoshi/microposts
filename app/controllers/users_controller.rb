2e7ab39139d6ec97c18d29b6f456c5b589ecde11
shimizu_akiyoshi <acho.0314@gmail.com>
2015-10-10 04:02:58 +0000

    submit a task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[ :success ] = "Comleted edit."
+      redirect_to edit_user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -21,7 +35,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :password, :location, :profile,
                                  :password_confirmation)
   end
 end
